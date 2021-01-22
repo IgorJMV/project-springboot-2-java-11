0bee7d30aae6e626400c24ebd67b305a88379b6c
Igor Marinho <igorjmv2000@gmail.com>
2021-01-22 15:40:28 -0300

    Exception handling - findById

---
+++ src/main/java/com/igorjmv2000/gmail/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.igorjmv2000.gmail.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+}
